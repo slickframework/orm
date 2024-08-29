414c1c23d3595fe5171c70682e51993ee32b028b
Filipe Silva <silvam.filipe@gmail.com>
2024-08-29 18:13:30 +0000

    Fixes configuration interface change on slick/webstack

--- config/services.php
+++ config/services.php
@@ -12,17 +12,17 @@ namespace Slick\Orm\Config\Services;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManagerInterface;
+use Slick\Configuration\ConfigurationInterface;
 use Slick\Di\ContainerInterface;
 use Slick\Orm\Infrastructure\Persistence\EntityManagerCollection;
 use Slick\Orm\Infrastructure\Persistence\EntityManagerFactory;
 use Slick\Orm\Infrastructure\Persistence\ManagerSettings;
-use Slick\WebStack\Infrastructure\ApplicationSettingsInterface;
 
 return [
     EntityManagerFactory::class => fn (ContainerInterface $container) => new EntityManagerFactory($container),
     EntityManagerCollection::class => function (ContainerInterface $container) {
         $entityManagers = new EntityManagerCollection();
-        $settings = $container->get(ApplicationSettingsInterface::class)->get('databases', []);
+        $settings = $container->get(ConfigurationInterface::class)->get('databases', []);
         $factory = $container->get(EntityManagerFactory::class);
         foreach ($settings as $name => $values) {
             /** @var EntityManagerInterface $entityManager */
@@ -31,7 +31,7 @@ return [
             $container->register("$name.entity.manager", $entityManager);
             $container->register("$name.db.connection", $entityManager->getConnection());
         }
-        $types = $container->get(ApplicationSettingsInterface::class)->get('types', []);
+        $types = $container->get(ConfigurationInterface::class)->get('types', []);
         foreach ($types as $name => $className) {
             Type::addType($name, $className);
         }
